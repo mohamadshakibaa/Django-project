4a97494ab56dda9db6aabdc10f01a08812a258a5
mohamadshakibaa <www.mohamadrasol123@gmail.com>
2025-12-28 02:06:36 +0330

    Forms & Validation 1

--- core/views.py
+++ core/views.py
@@ -1,31 +1,45 @@
 from django.shortcuts import render, get_list_or_404
 from django.http import HttpResponse
 from .models import Product
+from .forms import ProductForm
 
 def home(request):
     return render(request, 'core/home.html')
 
+
 def about(request):
     return render(request, 'core/about.html', {'message': ' helllllllllllo baby'})
 
-def products(request):
-    return render(request, 'core/products.html')
 
 def contact(request):
     return render(request, 'core/contact.html')
 
-def sample(request):
-    if request.method == 'POST':
-        return HttpResponse("POST received")
-    
-    return render(request, 'core/sample.html')
-
-def base(request):
-    return render(request, 'core/base.html')
-
-def title(request):
-    return render(request, 'core/title.html')
 
 def product_detail(request, product_id):
     product = get_list_or_404(Product, id=product_id)
     return render(request, 'core/product_detail.html', {"product": product})
+
+
+def product_list(request):
+    products = Product.objects.active()
+    return render(request, 'core/list.html', {"products": products})
+
+
+def activate_product(request, pk):
+    product = get_list_or_404(Product, id=pk)
+    product.is_available = True
+    product.save()
+    return HttpResponse("Activated")
+
+
+def product_create(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Product Created")
+    
+    else:
+        form = ProductForm()
+    
+    return render(request, 'core/product_form.html', {'form': form})
